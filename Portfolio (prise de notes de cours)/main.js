5b03eecdf44ad6964f0dd9b4d52865b1e9963ed2
Daf <all.daphnee@gmail.com>
2026-01-06 13:15:34 +0100

    Add files via upload

---
+++ Portfolio (prise de notes de cours)/main.js
@@ -0,0 +1,37 @@
+console.log("cc")
+
+let lifePoints = 100
+
+console.log(lifePoints)
+
+lifePoints += 90
+
+console.log(lifePoints)
+
+if (true) {
+    console.log("bonjour");
+}
+
+let age = 15
+
+if (age>=18) {
+    console.log("Tu es majeur");
+} else {
+    console.log("Tu es mineur");
+}
+
+let time = 13
+
+if(time >= 8 && time <=12) {
+    console.log("Magasin ouvert");
+}
+else if (time >= 14 && time <= 19) {
+    console.log("Magasin ouvert")
+}
+else {
+    console.log("Magasin fermé");
+}
+
+let Prénom = "Moi" 
+
+console.log(`Je m'appelle ${Prénom}, bonjour!`)
